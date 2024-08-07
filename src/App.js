6e46418181128c96c48680bb8e4178f8dfa52a35
w41k3r414n <tghanshyam512@gmail.com>
2024-08-07 18:44:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import List from './components/List';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header> <h1>NOTES SNIPPETS</h1></header>
+      <List />
+    </div>
+  );
+}
+
+
+export default App;
